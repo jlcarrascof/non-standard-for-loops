c52f4cdcda9a72dfa678bacd5b43e486e1b75e7d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-04 10:44:28 -0400

    Fix secondPower.js for testing purposes

--- secondPower.js
+++ secondPower.js
@@ -1,3 +1,4 @@
+/*
 function secondPower(arr) {
     const finalArray = [];
     for (let i = 0; i < arr.length; i++) {
@@ -10,3 +11,17 @@ function secondPower(arr) {
 
 result = secondPower([1, 2, 3, 4, 5, 6, 7, 8]);
 console.log(result);
+*/
+
+function secondPower(arr) {
+    const finalArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== 0 && (arr[i] & (arr[i] - 1)) === 0) {
+        finalArray.push(i);
+      }
+    }
+    return finalArray;
+  }
+
+  result = secondPower([1, 2, 3, 4, 5, 6, 7, 8]);
+  console.log(result); // [0, 1, 3, 7]
